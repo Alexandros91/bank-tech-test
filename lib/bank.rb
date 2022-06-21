db66a8bb9dbd077f6d75ce04423bcc22dfb236a2
alexandrospapagiannis <alpa31291@gmail.com>
2022-06-21 14:51:48 +0100

    Adding the show_balance method

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,4 +3,8 @@ class Bank
   def initialize(balance)
     @balance = balance
   end
+
+  def show_balance
+    @balance
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,11 @@ describe Bank do
   it 'initializes a bank account with a balance' do
     expect(Bank).to respond_to(:new).with(1).argument
   end
+
+  describe '#show_balance' do
+    it 'shows the balance' do
+      bank = Bank.new(0)
+      expect(bank.show_balance).to eq(0)
+    end
+  end
 end
